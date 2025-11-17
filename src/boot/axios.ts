4b6e2d07a9463a6e055f576750b85b940f480c69
Willian Deus <willian.deus@hitechnologies.com.br>
2025-11-17 20:58:47 -0300

    changed api

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -2,7 +2,7 @@ import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://sharepageapi.onrender.com'
 })
 
 api.interceptors.request.use((config) => {
